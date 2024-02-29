2e706d59df028810c9b5f2652375d6248415102e
runes121 <75335579+runes121@users.noreply.github.com>
2024-02-29 12:49:02 +0000

    fix????

--- AnimalGenerator/randomAnimalImg.js
+++ AnimalGenerator/randomAnimalImg.js
@@ -4,11 +4,14 @@ const loadButton = document.getElementById('loadRandomCat');
 async function fetchCatImage() {
   try {
     const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    console.log("getting photo")
     const data = await response.json();
     const imageUrl = data[0].url;
     catImage.src = imageUrl;
+    console.log("added photo")
   } catch (error) {
     console.error('Error fetching cat image:', error);
+    window.print("there was trouble getting the photo :(")
   }
 }
 
